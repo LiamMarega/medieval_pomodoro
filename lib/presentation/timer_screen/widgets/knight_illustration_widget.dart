7f1565c8c6a872485e4a37e46c66efaad24c7fc8
LiamMarega <liammarega85@gmail.com>
2025-08-18 19:43:49 -0300

    Refactor KnightIllustrationWidget to use GIF animation
    - Changed KnightIllustrationWidget from Stateless to Stateful to manage GIF animation.
    - Integrated gif package for improved animation handling.
    - Implemented GifController to control playback and looping of the knight GIF.
    - Enhanced loading experience with a CircularProgressIndicator while the GIF is being fetched.

--- lib/presentation/timer_screen/widgets/knight_illustration_widget.dart
+++ lib/presentation/timer_screen/widgets/knight_illustration_widget.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:medieval_pomodoro/core/widgets/inner_shadow.dart';
+import 'package:gif/gif.dart';
 
-class KnightIllustrationWidget extends StatelessWidget {
+class KnightIllustrationWidget extends StatefulWidget {
   const KnightIllustrationWidget({super.key});
+
+  @override
+  State<KnightIllustrationWidget> createState() =>
+      _KnightIllustrationWidgetState();
+}
+
+class _KnightIllustrationWidgetState extends State<KnightIllustrationWidget>
+    with TickerProviderStateMixin {
+  late final GifController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = GifController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +48,23 @@ class KnightIllustrationWidget extends StatelessWidget {
             ),
             child: InnerShadow(
               key: Key('shadow'),
-              child: Image.asset(
-                'assets/images/knight.gif',
-                fit: BoxFit.cover,
+              child: Gif(
+                image: const AssetImage('assets/images/knight.gif'),
+                controller: _controller,
+                duration: const Duration(
+                    seconds: 6), // Controla la velocidad de reproducción
+                autostart: Autostart.no, // No arranca automáticamente
+                placeholder: (context) => const Center(
+                  child: CircularProgressIndicator(),
+                ),
+                onFetchCompleted: () {
+                  // Cuando termina de cargar, arrancamos en bucle ida-vuelta
+                  _controller.repeat(
+                    min: 0,
+                    max: 1,
+                    reverse: true, // Esto hace el efecto ping-pong
+                  );
+                },
               ),
             ),
           ),
